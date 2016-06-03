f1be509b6962d26023d758346c666ce2128c0cfc
niyamath <m.niyamath.khan@gmail.com>
2016-06-02 20:10:25 -0400

    hello

--- a2-muhammad-khan/test9.c
+++
@@ -1,5 +0,0 @@
-#include "header9.h"
-/*  TODO:  For this problem, build a function called 'bzi' that takes a pointer to a pointer to a 'struct aqh' that represents the first node in a linked list, and a second item that represents the items we want delete.  Return a pointer to a new list where all of the elements in the list that match the second parameter have been deleted.  If the list only contains elements that have the value want to delete, return a null pointer.
-Check the header file to find out what the structure definition and function prototype looks like.
-*/
-	
--- data_structures/a2-muhammad-khan/test6.c
+++
@@ -1,5 +0,0 @@
-#include "header6.h"
-/*  TODO:  For this problem, build a function called 'jyn' that takes a pointer to a 'struct ncq that represents the first node in a linked list.  This function should print all the items in the list.
-Check the header file to find out what the structure definition and function prototype looks like.
-*/
-	
